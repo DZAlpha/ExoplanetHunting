1e6f70c20f1f2d0c051c0031ab6a6005b8349e3e
SWi <wieczorek0725@gmail.com>
2021-01-06 14:05:32 +0100

    plots.py for drawing some plots

--- dataread.py
+++ dataread.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pandas as pd
 
-# url_path ='https://raw.githubusercontent.com/DZAlpha/ExoplanetHunting/main/exoTest.csv'
-# df = pd.read_csv(url_path, index_col=0)
-
 def normalize(series):
     return (series - series.min()) / (series.max() - series.min())
 
@@ -32,16 +29,3 @@ def get_train_df():
 
 def get_test_df():
   return pd.read_csv('exoTest.csv', index_col = 0)
-
-
-
-# ID = np.arange(570)
-# label = df.index.values
-
-# multi_index = list(zip(ID,label))
-# index = pd.MultiIndex.from_tuples(multi_index, names=["ID", "LABEL"])
-# df = df.set_index(index)
-
-# # Normalized and standarized data frames
-# normalized_df = df.apply(normalize,axis=1)
-# standarized_df = df.apply(standarize, axis=1)
