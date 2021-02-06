6339bce21ea414915d4bfb0a93a20e17347872c5
Artuz37 <a.skarzynski7@gmail.com>
2021-02-06 17:05:53 +0100

    niechtodzialaprosze

--- featuregeneration.py
+++ featuregeneration.py
@@ -72,7 +72,19 @@ def fourier_outliers(fourier_df, right_index, span = 20, k = 3.5):
     return np.array(fourier_df.apply(lambda row: len(row[:right_index][bollinger_band(row[:right_index], span = span, k = k)[2]]), axis = 1))
 
 def n_outliers(row, span = 20, k = 3):
-    return len(row[bollinger_band(row, span = span, k = k)[3]])    
+    return len(row[bollinger_band(row, span = span, k = k)[3]]) 
+
+def out_idx(row):
+    return np.diff(np.where(bollinger_band(row, span=20, k=2.5)[3])[0])
+
+def d_std(series):
+    if len(series)>1:
+        return series.std()
+    return 3197.
+def d_mean(series):
+    if len(series)>1:
+        return series.mean()
+    return 0.
     
 class Features:
     def __init__(self, df, standarized_df):
@@ -86,7 +98,9 @@ class Features:
         self.features['Variance'] = np.array(self.df.apply(lambda row: row.var(), axis = 1))
         self.features['Mean'] = np.array(self.df.apply(lambda row: row.mean(), axis = 1))
         self.features['Outliers'] = np.array(self.df.apply(lambda row: n_outliers(row), axis=1))
-        
+        idx = self.df.apply(lambda row: out_idx(row), axis=1)
+        self.features['std of outliers'] = np.array(idx.apply(lambda row: d_std(row)))
+        self.features['mean of outliers'] = np.array(idx.apply(lambda row: d_mean(row)))
         self.features['Fourier std'] = self.dft.std
         self.features['Fourier mean'] = self.dft.mean
         self.features['Fourier spikes'] = self.dft.spikes
